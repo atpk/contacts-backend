55898cd4f1fe91abd0e357eda3bd2dd8e98a3a9b
Pranav <pranavkanire@gmail.com>
2023-07-26 12:36:05 +0530

    url change

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use("/api/contacts", require("./routes/contactRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
+app.use("/contacts", require("./routes/contactRoutes"));
+app.use("/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
